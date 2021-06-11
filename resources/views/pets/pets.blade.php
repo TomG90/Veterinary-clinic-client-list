d8e87ae9f1999f4da8022d82408eed1602ccae98
petrKavulok <petr.kavulok@gmail.com>
2021-06-11 16:31:50 +0200

    css added

--- resources/views/pets/pets.blade.php
+++ resources/views/pets/pets.blade.php
@@ -5,15 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pet List</title>
+    <link rel="stylesheet" href="/CSS/style.css">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;900&display=swap" rel="stylesheet">
 </head>
 <body>
 <ul>
     @foreach($pets as $pet)
     <li>
-    <h2><a href="{{ action('PetsController@index') }}/{{$pet->id}}">{{ $pet->name }}</a></h2>
-    <img src="{{ $pet->img }}" alt="">
-    <p>{{ $pet->client->surname }}</p>
-    
+
+        <div class="card">
+            <div class="img">
+                <img src="{{ $pet->img }}" alt="">
+            </div>
+            <div class="info">
+                <h2>Good puppy <a href="{{ action('PetsController@index') }}/{{$pet->id}}">{{ $pet->name }}</a></h2>
+                <p> Owner: Mr./Ms. {{ $pet->client->surname }}</p>
+            </div>
+        </div>
     </li>
     @endforeach
     </ul>
--- resources/views/pets/petsdetails.blade.php
+++ resources/views/pets/petsdetails.blade.php
@@ -4,13 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="\css\details.css">
+    <title>{{ $details->name}}</title>
 </head>
 <body>
     <h1>{{ $details->name}}</h1>
-    <img src="{{ $details['img']}}" alt="">
+    <h3>(Owner: {{ $details->client->surname }})</h3>
+    <img class="doggo-pic" src="{{ $details['img']}}" alt="">
+
+    <h3>Symptoms</h3>
     {{ $details['symptoms']}}
+    <h3>Medical History</h3>
     {{ $details['medical_history']}}
-    {{ $details->client->surname }}
 </body>
 </html>
